9e61b7a0f5661cb6d4442b693335c290f466425d
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2025-12-19 03:00:44 +0530

    Update backend.tf

--- azure/backend.tf
+++ azure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "cisco-raman-terraform-state-bucket"
-    key            = "ci/aws/terraform.tfstate"
+    key            = "ci/azure/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cisco-raman-terraform-state-lock"
     encrypt        = true
